d8d00b6aecf37c51a01be5c5f5b1e4be9bc55036
Evan Dyce <endyce@gmail.com>
2021-09-05 01:05:38 -0700

    Basic database integration

--- src/flaskblog.py
+++ src/flaskblog.py
@@ -2,10 +2,44 @@ from flask import Flask, render_template, url_for, request
 from flask.helpers import flash
 from werkzeug.utils import redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3d0b6dad9e09995ed085ffacff2f7173'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+# class represents table in sql each one is a column
+# this is user table 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    # creates one to many relationship with Post column 
+    # uses foreign key to get all posts made by the user
+    # not a column, just a seperate query
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}'"
+
 
 
 posts = [
@@ -26,6 +60,12 @@ posts = [
         'title' : 'Flask', 
         'content' : 'I\'m making a flask thing',
         'date_posted' : 'August 19, 2021'
+    }, 
+    {
+        'author' : 'Josh Dyce', 
+        'title' : 'Blog Post 4', 
+        'content' : 'I am so bad at golf and also suck at catan',
+        'date_posted' : 'August 19, 2021'
     }
 ]
 
@@ -40,17 +80,24 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    # makes form from forms.py
     form = RegistrationForm()
+
+    # if it validates when posted flash message and return to home
     if form.validate_on_submit():
         flash(f'Account Created for {form.username.data}', category='success')
         return redirect(url_for('home'))
     
+    # if not re render the register.html file
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    # gets LoginForm
     form = LoginForm()
 
+    # if it validates it chcks emails and flashes success and loads redirect
+    # else flashes fail and reloads the login.html page
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
             flash('You have been logged in', 'success')
