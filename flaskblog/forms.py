c18a59191879914a3970e8d0c28f5efed02c3830
Evan Dyce <endyce@gmail.com>
2021-09-12 22:29:32 -0700

    Allows user to update their information

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Length, Email, ValidationError
 from flaskblog.models import User
@@ -44,3 +46,34 @@ class LoginForm(FlaskForm):
     
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email(message='Invalid Email')])
+
+    image = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+
+            # if there is a user in the database with the same username will return ite
+            # else it will just be null and conditional is Falsey
+            user = User.query.filter_by(username=username.data).first()
+        
+            if user:
+                raise ValidationError('Username is already taken. Choose a new one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            # if there is a user in the database with the same email will return ite
+            # else it will just be null and conditional is Falsey
+            user = User.query.filter_by(email=email.data).first()
+        
+            if user:
+                raise ValidationError('Account with this email already exists.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,10 +1,13 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, redirect, request
 from flask.helpers import flash
 from flask_login import login_user, current_user, logout_user
 from flask_login.utils import login_required
 
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import Post, User
 
 
@@ -80,7 +83,10 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
+            # gets next_page value from the url if it is there
+            # is present for the login_required when it sends to login 
             next_page = request.args.get('next')
+            # ternary returns next_page if it exists else it returns home page 
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Failed', 'danger')
@@ -93,8 +99,45 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    '''
+    save users uploaded picture into filesystem and returns generated file name
+    '''
+    random_hex = secrets.token_hex(8)
+    _, fext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + fext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-@app.route("/account")
+    # resize image to fit better and stop big files from taking up space
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+
+    # if it is a post request and it is validated 
+    # update database with the data from the forms and redirect back to the account page 
+    # with a get request which then displays the new data in the data fields
+    if form.validate_on_submit():
+        if form.image.data:
+            filename = save_picture(form.image.data)
+            current_user.image_file = filename
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+  
+        flash('Your account has been updated', category='success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename=f'profile_pics/{current_user.image_file}')
+    return render_template('account.html', title='Account', image=image_file, form=form)
